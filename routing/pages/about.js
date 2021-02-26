f78dc47fee535349fae18f81f2fb6425ecc75b33
Jeesoo Min <jeesoo.min@samsung.com>
2021-02-26 14:17:38 +0900

    SPA routing with JavaScript

---
+++ routing/pages/about.js
@@ -0,0 +1,3 @@
+export default function About() {
+  return `<div class="page"><h1>About Page</h1></div>`;
+}
---
+++ routing/pages/home.js
@@ -0,0 +1,3 @@
+export default function Home() {
+  return `<div class="page"><h1>Home Page</h1></div>`;
+}
